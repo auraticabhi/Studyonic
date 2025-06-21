3d2dcb9fe79913e0d886f66e42aa7fb78880b291
auraticabhi <abhijeetgupta989@gmail.com>
2025-06-22 00:59:33 +0530

    feat: Add healthchecks for all services and frontend retry logic

--- studyonic-frontend/src/services/apiconnector.js
+++ studyonic-frontend/src/services/apiconnector.js
@@ -1,6 +1,24 @@
 import axios from "axios";
+import axiosRetry from 'axios-retry';
 
 export const axiosInstance = axios.create({});
+
+axiosRetry(axiosInstance, {
+    retries: 3,
+    retryDelay: (retryCount) => {
+        console.log(`Retrying request, attempt #${retryCount}`);
+        return retryCount * 1000; // Time interval between retries
+    },
+    retryCondition: (error) => {
+        // Retry on network errors or 5xx server errors
+        return (
+            axiosRetry.isNetworkError(error) ||
+            error.response.status === 503 ||
+            error.response.status === 502 ||
+            error.response.status === 504
+        );
+    },
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
